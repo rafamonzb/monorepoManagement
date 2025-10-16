6a50198cac5d6ac7d5b0d772beedbf47906c3203
rafamonzb <rafael.monzo-bravo@capgemini.com>
2025-10-16 10:08:54 +0200

    Change npm install deps command

--- packages/project2/src/functions.cjs
+++ packages/project2/src/functions.cjs
@@ -1,5 +1,5 @@
 // const button = document.getElementById("button");
-// Add a change in a project IV
+// Add a change in a project V
 
 const currentNameE1 = document.getElementById("current-name");
 const nextNameE1 = document.getElementById("next-name");
