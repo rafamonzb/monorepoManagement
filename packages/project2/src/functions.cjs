2a225bcbfcb185ac5b188c09ae032aff52302cbc
rafamonzb <rafael.monzo-bravo@capgemini.com>
2025-10-16 09:50:07 +0200

    Add change to project

--- packages/project2/src/functions.cjs
+++ packages/project2/src/functions.cjs
@@ -1,5 +1,5 @@
 // const button = document.getElementById("button");
-// Add a change in a project
+// Add a change in a project (Again)
 
 const currentNameE1 = document.getElementById("current-name");
 const nextNameE1 = document.getElementById("next-name");
