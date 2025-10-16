fbbdc47401a6465e433cc7a3873068af289a653e
rafamonzb <rafael.monzo-bravo@capgemini.com>
2025-10-16 10:19:37 +0200

    It was an epiphany, good job me

--- packages/project2/src/functions.cjs
+++ packages/project2/src/functions.cjs
@@ -1,5 +1,5 @@
 // const button = document.getElementById("button");
-// Add a change in a project VII
+// Add a change in a project VIII
 
 const currentNameE1 = document.getElementById("current-name");
 const nextNameE1 = document.getElementById("next-name");
