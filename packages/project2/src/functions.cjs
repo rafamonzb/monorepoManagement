e24ca85a4cb71e84b381fa73fc422c1bc172cdc7
rafamonzb <rafael.monzo-bravo@capgemini.com>
2025-10-20 10:42:43 +0200

    I'm not even sure

--- packages/project2/src/functions.cjs
+++ packages/project2/src/functions.cjs
@@ -1,5 +1,5 @@
 // const button = document.getElementById("button");
-// Add a change in a project XV
+// Add a change in a project XVI
 
 const currentNameE1 = document.getElementById("current-name");
 const nextNameE1 = document.getElementById("next-name");
