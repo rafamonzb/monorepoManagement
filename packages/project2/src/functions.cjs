f3bd84b302b4f53f992c0271fd8d75c2852ae5a2
rafamonzb <rafael.monzo-bravo@capgemini.com>
2025-10-20 11:10:33 +0200

    .

--- packages/project2/src/functions.cjs
+++ packages/project2/src/functions.cjs
@@ -1,5 +1,5 @@
 // const button = document.getElementById("button");
-// Add a change in a project XVIII
+// Add a change in a project XIX
 
 const currentNameE1 = document.getElementById("current-name");
 const nextNameE1 = document.getElementById("next-name");
